39f4dc0821f29430e196099e2996d23b414a15f0
Abner Guimaraes <guimaraesabner@gmail.com>
2025-02-19 21:06:33 -0300

    Fix images bug

--- app/fotos/page.tsx
+++ app/fotos/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import fer1 from '@/public/fernando1.jpg';
+import fer2 from '@/public/fernando2.jpg';
+
 function fotos() {
   return (
     <section>
-      horários
+      <Image src={fer1} alt='fernando'/>
+      <Image src={fer2} alt='fernando'/>
     </section>
   )
 }
